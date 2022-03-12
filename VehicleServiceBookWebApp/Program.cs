d17c94a5381371c2bb488de098b9913d94aa8f90
Angel Raychev <angelraychev@gmail.com>
2022-03-12 18:26:39 +0200

    Service collection extension methods

--- VehicleServiceBookWebApp/Program.cs
+++ VehicleServiceBookWebApp/Program.cs
@@ -1,17 +1,13 @@
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using VehicleServiceBook.Core.Constants;
 using VehicleServiceBook.Infrastructure.Data;
+using VehicleServiceBookWebApp.Extensions;
 using VehicleServiceBookWebApp.ModelBinders;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
+builder.Services.AddApplicationDbContexts(builder.Configuration);
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews()
