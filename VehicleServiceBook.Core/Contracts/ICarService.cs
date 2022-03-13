2057431fa37948ce6d4c9b033355d70d122bfbe1
Angel Raychev <angelraychev@gmail.com>
2022-03-13 12:08:58 +0200

    Layout page fixed

---
+++ VehicleServiceBook.Core/Contracts/ICarService.cs
@@ -0,0 +1,10 @@
+﻿using VehicleServiceBook.Core.ViewModels;
+
+namespace VehicleServiceBook.Core.Contracts
+{
+    public interface ICarService
+    {
+        public void CreateCar(AddCarViewModel model);
+    }
+
+}
---
+++ VehicleServiceBook.Core/ViewModels/AddCarViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VehicleServiceBook.Core.ViewModels
+{
+    public class AddCarViewModel
+    {
+     
+        [Required]
+        [StringLength(20,MinimumLength = 5,ErrorMessage = "{0} must be between {2} and {1} characters")]
+        public string Model { get; set; }
+
+        
+        [StringLength(4,MinimumLength = 4,ErrorMessage = "{0} must be {1} characters")]
+        public string Year { get; set; }
+
+        [Required]
+        [StringLength(17,MinimumLength = 17,ErrorMessage = "{0} must be {1} characters")]
+        public string VinNumber { get; set; }
+    }
+}
